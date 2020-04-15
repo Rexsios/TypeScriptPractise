9e1cef8cdc207218693ffec4e85f8e81c63420a5
Slawomir Palka <slawomirpalka97@gmail.com>
2020-04-15 18:22:41 +0200

    125 Creating a re-usable validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+//Validation
+
+interface Validatable {
+    value: string | number
+    required?: boolean
+    minLength?: number
+    maxLength?: number
+    min?: number
+    max?: number
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0
+    }
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength
+    }
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value >= validatableInput.min
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value <= validatableInput.max
+    }
+    console.log("jest?" + isValid)
+    return isValid
+}
+
 //autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value
@@ -39,10 +71,23 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value
         const enteredPeople = this.peopleInputElement.value
 
-        if (enteredTitle.trim().length === 0 ||
-            enteredDescription.trim().length === 0 ||
-            enteredPeople.trim().length === 0
-        ) {
+        const titleValidatabe: Validatable = {
+            value: enteredTitle,
+            required: true
+        }
+        const descriptionValidatabe: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        }
+        const peopleValidatabe: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+        if (!validate(titleValidatabe) || !validate(descriptionValidatabe) || !validate(peopleValidatabe)) {
             alert('Invalid input, pleas try again!')
             return;
         } else {
